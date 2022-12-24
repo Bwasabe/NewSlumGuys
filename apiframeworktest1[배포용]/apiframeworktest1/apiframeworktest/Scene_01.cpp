a0cb6601d99be25e637387cdc9cf467c8c1d7f22
hanmin0728 <gksalsdud1234@gmail.com>
2022-12-24 17:28:58 +0900

    나일했다

--- apiframeworktest1[배포용]/apiframeworktest1/apiframeworktest/Scene_01.cpp
+++ apiframeworktest1[배포용]/apiframeworktest1/apiframeworktest/Scene_01.cpp
@@ -9,6 +9,7 @@
 #include "PlayerManager.h"
 #include "CircleRenderMgr.h"
 #include "BaseEnemy.h"
+#include "FaceObj.h"
 Scene_01::Scene_01()
 {
 }
@@ -41,6 +42,10 @@ void Scene_01::Enter()
 	AddObject(baseEnemy, GROUP_TYPE::MONSTER);
 
 
+	Object* faceObj = new FaceObj(Vec2(float((Core::GetInst()->GetResolution().x / 2)), (Core::GetInst()->GetResolution().y / 2) * 1.f), FaceState::Smile);
+	faceObj->SetScale(Vec2(300.f, 300.f));
+	AddObject(faceObj, GROUP_TYPE::UI);
+
 	//// 충돌 지정 
 	//// Player - Monster 그룹 간의 충돌 체크
 	CollisionMgr::GetInst()->CheckGroup(GROUP_TYPE::PLAYER, GROUP_TYPE::MONSTER);
@@ -58,6 +63,8 @@ void Scene_01::Enter()
 
 	cObj->SetScale(Vec2(100.f, 100.f));
 	AddObject(cObj, GROUP_TYPE::PLAYER);*/
+
+	int hp = 30;
 
 	
 	int index = 19;
@@ -110,373 +117,7 @@ void Scene_01::Enter()
 	float angleTable14[12] = { 0.f,30.f, 60.f, 90.f, 120.f, 150.f, 180.f, 210.f, 240.f, 270.f, 300.f, 330.f };
 	bool isIn14[12] = { false, false , true ,true , false , true, true, false, false, false, true, true };
 
-	switch (index)
-	{
-	case 0:
-		for (int i = 0; i < 4; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
 
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn0[i]);
-
-			circle->SetPosToCirclePos(angleTable0[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 1:
-		for (int i = 0; i < 12; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn1[i]);
-
-			circle->SetPosToCirclePos(angleTable1[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 2:
-		//PlayerManager() pObj->SetIsGoRight(true);
-		//pObj2->SetIsGoRight(false);
-		//방향전환
-		break;
-	case 3:
-	{
-		//Object* tObj = new Circle;
-		//tObj->SetPos(Vec2(Core::GetInst()->GetR5esolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-		//tObj->SetScale(Vec2(300.f, 300.f));
-		//AddObject(tObj, GROUP_TYPE::MONSTER);
-	}
-	//돌아가는 공
-	break;
-	case 4:
-		for (int i = 0; i < 6; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn2[i]);
-
-			circle->SetPosToCirclePos(angleTable2[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-	case 5:
-		//방향전환
-		break;
-	case 6:
-		for (int i = 0; i < 6; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn3[i]);
-
-			circle->SetPosToCirclePos(angleTable3[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 7:
-		//순간이동
-		break;
-	case 8:
-		for (int i = 0; i < 10; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn4[i]);
-
-			circle->SetPosToCirclePos(angleTable4[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-
-	case 9:
-		for (int i = 0; i < 8; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-		///	circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn5[i]);
-
-			circle->SetPosToCirclePos(angleTable5[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 10:
-		//돌아가는공
-		break;
-	case 11:
-		//방향전환
-		break;
-	case 12:
-		for (int i = 0; i < 6; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn6[i]);
-
-			circle->SetPosToCirclePos(angleTable6[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-
-	case 13:
-		//돌아가는 공
-		break;
-
-	case 14:
-		//순간이동
-		break;
-
-	case 15:
-		for (int i = 0; i < 4; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn7[i]);
-
-			circle->SetPosToCirclePos(angleTable7[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 16:
-		//순간이ㅣ동
-		break;
-	case 17:
-		//방향전환
-		break;
-	case 18:
-		for (int i = 0; i < 3; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn8[i]);
-
-			circle->SetPosToCirclePos(angleTable8[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-
-		}
-		break;
-	case 19:
-		for (int i = 0; i < 3; ++i)
-		{
-
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn9[i]);
-
-			circle->SetPosToCirclePos(angleTable9[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 20:
-		//돌아가는공
-		break;
-
-	case 21:
-		//돌아가는공
-		break;
-	case 22:
-		//방향전환
-		break;
-	case 23:
-		for (int i = 0; i < 5; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn10[i]);
-
-			circle->SetPosToCirclePos(angleTable10[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-
-	case 24:
-		//방향전환
-		break;
-	case 25:
-		for (int i = 0; i < 7; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn11[i]);
-
-			circle->SetPosToCirclePos(angleTable11[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 26:
-		for (int i = 0; i < 7; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn12[i]);
-
-			circle->SetPosToCirclePos(angleTable12[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 27:
-		//순간이동
-		break;
-	case 28:
-		for (int i = 0; i < 5; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn13[i]);
-
-			circle->SetPosToCirclePos(angleTable13[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	case 29:
-		//방향전환
-		break;
-	case 30:
-		for (int i = 0; i < 12; ++i)
-		{
-			BaseCircle* circle = new BaseCircle;
-			circle->SetStartPos(CircleMgr::GetInst()->GetCirclePos());
-			circle->SetScale(Vec2(15.f, 15.f));
-			//circle->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
-			//circle->SetMyRadius(15);
-
-			//circle->SetCurrentRadius(CircleMgr::GetInst()->GetInnerRadius() - circle->GetMyRadius() * 0.5f);
-			circle->SetInOrOutCircle(isIn14[i]);
-
-			circle->SetPosToCirclePos(angleTable14[i] * DEG2RAD);
-			circle->SetColor(RGB(0, 0, 0));
-			circle->SetAlpha(255);
-			//SceneMgr::GetInst()->GetCurScene()->AddObject(circle, GROUP_TYPE::PLAYER);
-			//circle->CreateCollider(COLLIDER_TYPE::CIRCLE);
-			CircleRenderMgr::GetInst()->AddCircle(circle);
-		}
-		break;
-	default:
-		break;
-	}
 
 }
 
@@ -493,4 +134,9 @@ void Scene_01::Update()
 	{
 		ChangeScene(SCENE_TYPE::START);
 	}*/
+	int 
+	if (BaseEnemy::GettHP())
+	{
+
+	}
 }
